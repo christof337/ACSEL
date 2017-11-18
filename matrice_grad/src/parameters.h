17bcb161d32c2694f7155357dc0d4f0978dac4cf
Christophe Pont <christof337@hotmail.fr>
2017-11-18 12:58:20 +0100

    Documenting parameters.h

--- matrice_grad/src/parameters.h
+++ matrice_grad/src/parameters.h
@@ -3,11 +3,17 @@
 
 #include <mpfr.h>
 
+/**
+ * @brief      List the different matrix types handled by the program
+ */
 enum matrixTypeEnum {
 	EXPONENTIAL,
 	HILBERT
 };
 
+/**
+ * @brief      List the different rounding mode types handled by the program
+ */
 enum roundingModeEnum {
 	RNDN, // round to nearest (roundTiesToEven in IEEE 754-2008),
 	STOCHASTIC, // custom ;)
@@ -17,6 +23,9 @@ enum roundingModeEnum {
     RNDA  // round away from zero. 
 };
 
+/**
+ * @brief      Union used to access the default value, which can have different type depending on the parameter used
+ */
 union defaultValueUnion {
 	double d;
 	enum matrixTypeEnum mte;
@@ -24,8 +33,16 @@ union defaultValueUnion {
 	enum roundingModeEnum rme;
 };
 
+/**
+ * @brief      Enum listing the different types of the default value of the params
+ */
 enum type {DOUBLE, MATRIXTYPEENUM, LONGINT, ROUNDINGMODEENUM};
 
+/**
+ * @brief      Main structure of this file. Contains all of the fields used to
+ *             write and access the command line parameters of the program.
+ *             Handle the default values.
+ */
 struct Param {
    char  name[50];
    char shortName[10];
@@ -37,6 +54,9 @@ struct Param {
    union defaultValueUnion currentValue;
 };
 
+/**
+ * @brief      List the different parameters used by this program.
+ */
 enum paramsEnum { MATRIX_SIZE, NB_ITER, MAX_PREC, ROUNDING_MODE, MATRIX_TYPE};
 
 int initParams(char * appName) ;
@@ -56,7 +76,6 @@ enum roundingModeEnum stringToRoundingModeEnum(char * string, const int size) ;
 mpfr_rnd_t roundingModeEnumToMpfrRndT(enum roundingModeEnum e) ;
 
 struct Param getParamFromParamEnum(enum paramsEnum pe) ;
-
 
 int handleParams( int argc, char *argv[] ) ;
 
