a4c5abc5b5e473bc60b36693b913ece77ca51063
Christophe Pont <christof337@hotmail.fr>
2018-02-14 17:31:52 +0100

    updating the original algorithm. TODO : save the value for every iteration (or for one iteration over 5 for instance)

--- Matlab/computeMultipleMassiveDiff.m
+++ Matlab/computeMultipleMassiveDiff.m
@@ -13,47 +13,72 @@ StochasticStr = 'STOCHASTIC';
 delimiterIn = '\t';
 headerlinesIn = 1;
 
-INIT = 2;
+MIN_PRECISION = 4;
+MAX_PRECISION = 200;
 
 % k = 2;
-k = INIT; % override
+k = MIN_PRECISION; % override
 shouldContinue = 1;
+MAX_NB_STOCH_FILES = 50;  % I have ran it 161 times
+NB_STOCH_FILES_TAKEN = 50;
+
+NB_BENCH_ZEROS = 10; % stop reading files as soon as for 10 precisions in a row the computed relDiff is 0
 
 referenceFileName = 'lorenzRef_pre=200_rm=RNDN.dat';
 refArray = importdata(strcat(RNDNFolder,'/',referenceFileName),delimiterIn,headerlinesIn);
 
-RNDNDiff = zeros(200,2);
-StochasticDiff = zeros(200,2,200);
+RNDNDiff = zeros(MAX_PRECISION-MIN_PRECISION+1,2);
+StochasticDiff = zeros(MAX_PRECISION-MIN_PRECISION+1,2,NB_STOCH_FILES_TAKEN);
 
+permutations = randperm(MAX_NB_STOCH_FILES);%,NB_STOCH_FILES_TAKEN); % taking NB_STOCH_FILES_TAKEN files randomly
+
+nbZeros = 0;
+% loop over precisions
 while ( shouldContinue )
+    index = k-MIN_PRECISION+1;
+
     precisionStr = int2str(k);
     % file name construction
     fileNameRNDN = strcat(RNDNFolder,'/',fileNamePrefix,precisionStr,fileNameSuffix1,RNDNStr,fileNameSuffix2, fileNameSuffix3);
-    
+
     % opening the file to check that it exists
     fileIDRNDN = fopen(fileNameRNDN, 'r');
-    
+
     if (fileIDRNDN == -1)
         % if not, we're running out of files ==> exit
         shouldContinue = 0; % exit loop
         disp('End RNDN');
-        disp(precisionStr);
+        %disp(precisionStr);
+    elseif (nbZeros >= NB_BENCH_ZEROS)
+        disp('filling with zeros...')
+        shouldContinue = 0; % exit loop
+        % filling with zeros
+        RNDNDiff(index:size(RNDNDiff,1),:) = horzcat(permute(k:MAX_PRECISION,[2 1]),zeros(size(RNDNDiff,1)-index+1,1));
+        for cpt = 1:NB_STOCH_FILES_TAKEN
+            StochasticDiff(index:size(StochasticDiff,1),:,cpt) = horzcat(permute(k:MAX_PRECISION,[2 1]),zeros(size(StochasticDiff,1)-index+1,1));
+        end
     else
-        index = k-INIT+1;
         fclose(fileIDRNDN); % do not need it anymore
-        
+
         % importing array
         RNDNArray = importdata(fileNameRNDN,delimiterIn,headerlinesIn);
         % computing the diff with the reference file
-        tmp1 = [k computeRelativeDiff(refArray,RNDNArray)];
+        relDiff = computeRelativeDiff(refArray,RNDNArray)
+        tmp1 = [k relDiff];
         % saving the result in RNDN Diff
         %RNDNDiff = [RNDNDiff;tmp1];
         RNDNDiff(index,:) = tmp1;
         
+        if (relDiff == 0)
+            nbZeros = nbZeros + 1;
+        else
+            nbZeros = 0;
+        end
+        
         isStochFilesLeft = 1;
         l = 1;
-        while ( isStochFilesLeft && l < 20)
-            itStr = int2str(l-1);
+        while ( isStochFilesLeft && l < NB_STOCH_FILES_TAKEN)
+            itStr = int2str(permutations(l));
             fileNameStochastic = strcat(stochasticFolder,'/',fileNamePrefix,precisionStr,fileNameSuffix1,StochasticStr,fileNameSuffix2,'(',itStr,')',fileNameSuffix3);
             fileIDStochastic = fopen(fileNameStochastic, 'r');
             if (fileIDStochastic == -1)
@@ -64,7 +89,7 @@ while ( shouldContinue )
                 
                 StochasticArray = importdata(fileNameStochastic,delimiterIn,headerlinesIn);
                 tmp2 = [k computeRelativeDiff(refArray,StochasticArray)];
-                StochasticDiff(k-1,:,l) = tmp2;
+                StochasticDiff(index,:,l) = tmp2;
                 
                 l = l + 1;
                 
@@ -73,25 +98,16 @@ while ( shouldContinue )
         end
         
         disp(strcat('[',precisionStr,'] : RNDN       = ',num2str(tmp1(:,2))));
-        disp(strcat('[',precisionStr,'] : Stochastic = ',num2str(mean(StochasticDiff(k-1,2,:),3)))); % k*(l-1):((k+1)*l-1),2:3)))));
+        disp(strcat('[',precisionStr,'] : Stochastic = ',num2str(mean(StochasticDiff(index,2,:),3)))); % k*(l-1):((k+1)*l-1),2:3)))));
         disp('-');
         
         k = k + 1;
     end
 end
 
-% disp('--------------------------');
-% disp('RNDN Diff :');
-% disp(RNDNDiff);
-% disp('Stochastic Diff :');
-% disp(StochasticDiff);
-% 
-% disp(mean(RNDNDiff,'omitnan'));
-% disp(mean(StochasticDiff,'omitnan'));
-
 outputFolder = 'data/';
-dlmwrite(strcat(outputFolder,'RNDN_dif_2.dat'),RNDNDiff,'\t');
-dlmwrite(strcat(outputFolder,'STOCHASTIC_dif_correct_mean_2.dat'), ...
+dlmwrite(strcat(outputFolder,'RNDN_dif_massive_1.dat'),RNDNDiff,'\t');
+dlmwrite(strcat(outputFolder,'STOCHASTIC_dif_massive_1.dat'), ...
 	[ StochasticDiff(:,1), 	... precision
 	mean(					...
 		StochasticDiff(  	...
@@ -101,15 +117,15 @@ dlmwrite(strcat(outputFolder,'STOCHASTIC_dif_correct_mean_2.dat'), ...
 		, 3 ) 	... group the third dimension
 	]	...
 );
-% dlmwrite('STOCHASTIC_dif_multiple_1.dat',StochasticDiff,'\t');
-% dlmwrite('STOCHASTIC_dif_mean_0.dat',mean(StochasticDiff),'\t');
-% dlmwrite('STOCHASTIC_dif_mean_1.dat',mean(StochasticDiff,1),'\t');
-% dlmwrite('STOCHASTIC_dif_mean_2.dat',mean(StochasticDiff,2),'\t');
-% dlmwrite('STOCHASTIC_dif_mean_3.dat',mean(StochasticDiff,3),'\t');
 
-clear fileNamePrefix fileNameSuffix1 fileNameSuffix2 fileNameSuffix3 delimiterIn headerlinesIn k l shouldContinue
-clear fileNameStochastic fileNameRNDN stochasticFolder RNDNFolder isStochFilesLeft itStr INIT index
-clear precisionStr fileIDRNDN fileIDStochastic A tmp1 tmp2 refArray referenceFileName RNDNArray RNDNStr StochasticArray StochasticStr tmp
+disp('Exiting program normally...');
+
+clear fileNamePrefix fileNameSuffix1 fileNameSuffix2 fileNameSuffix3 ...
+    delimiterIn headerlinesIn k l shouldContinue fileNameStochastic ...
+    fileNameRNDN stochasticFolder RNDNFolder isStochFilesLeft itStr ...
+    INIT index precisionStr fileIDRNDN fileIDStochastic A tmp1 tmp2 ...
+    refArray referenceFileName RNDNArray RNDNStr StochasticArray ...
+    StochasticStr tmp relDiff;
 % clear RNDNDiff StochasticDiff
 
 % fileNameFolder = '/home/kito/Dev/Sources/ACSEL/matrice_grad/stochFiles/output';
